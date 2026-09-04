07be3fbb0a77c1651e34ef3c1359682ba53606d2
Peter Tecnet <petertecnet@gmail.com>
2026-09-03 22:46:13 -0300

    feat(contracts): enable Locaio V3 contract workflow

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './AppV2.jsx';
+import App from './AppV3.jsx';
 import PeterAccountGateway from './components/PeterAccountGateway.jsx';
 import { API_BASE_URL, APP_SLUG } from './services/api.js';
 import { installAuthenticatedDownloads } from './services/downloadBridge.js';
@@ -12,6 +12,7 @@ import './brand.css';
 import './visual-enhancements.css';
 import './dashboard-overview.css';
 import './design-system-v2.css';
+import './contract-workflow.css';
 
 installAuthenticatedDownloads();
 installProductionGuards();
