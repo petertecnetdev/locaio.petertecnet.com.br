626e75d326462efd8deb3df02fd115610d28e391
Peter Tecnet <petertecnet@gmail.com>
2026-09-03 23:00:37 -0300

    feat(ops): activate intelligent operations workspace

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './AppV2.jsx';
+import OperationsExperience from './components/OperationsExperience.jsx';
 import PeterAccountGateway from './components/PeterAccountGateway.jsx';
 import { API_BASE_URL, APP_SLUG } from './services/api.js';
 import { installAuthenticatedDownloads } from './services/downloadBridge.js';
@@ -14,6 +15,7 @@ import './visual-enhancements.css';
 import './dashboard-overview.css';
 import './design-system-v2.css';
 import './contract-profile-enhancements.css';
+import './operations-experience.css';
 
 installAuthenticatedDownloads();
 installProductionGuards();
@@ -23,7 +25,9 @@ installContractProfileEnhancements();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <PeterAccountGateway apiBaseUrl={API_BASE_URL} appSlug={APP_SLUG}>
-      <App />
+      <OperationsExperience>
+        <App />
+      </OperationsExperience>
     </PeterAccountGateway>
   </StrictMode>,
 );
