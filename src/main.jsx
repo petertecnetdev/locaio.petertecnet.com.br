de43620cb6192121334cfaf98aec97dfbd006d63
Peter Tecnet <petertecnet@gmail.com>
2026-09-03 22:47:31 -0300

    style: carrega camada avançada de navegação

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import './styles.css';
 import './auth-enhancements.css';
 import './brand.css';
 import './sidebar-redesign.css';
+import './navigation-enhancements.css';
 import './navigationEnhancements.js';
 
 installAuthenticatedDownloads();
